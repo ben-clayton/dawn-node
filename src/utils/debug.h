362e3383aefcb79a5b1cc31fa3cf145def4b5413
Ben Clayton <bclayton@google.com>
2021-09-15 18:14:49 +0100

    Log all interop calls

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -14,25 +14,74 @@
 }
 
 inline std::ostream& Write(std::ostream& out) { return out; }
-
+template <typename T>
+inline std::ostream& Write(std::ostream& out, const std::optional<T>& value);
+template <typename T>
+inline std::ostream& Write(std::ostream& out, const std::vector<T>& value);
+template <typename K, typename V>
+inline std::ostream& Write(std::ostream& out,
+                           const std::unordered_map<K, V>& value);
+template <typename... TYS>
+inline std::ostream& Write(std::ostream& out,
+                           const std::variant<TYS...>& value);
 template <typename VALUE>
-inline std::ostream& Write(std::ostream& out, VALUE&& value) {
-  return out << value;
-}
+inline std::ostream& Write(std::ostream& out, const VALUE& value);
 
 template <typename T>
-inline std::ostream& Write(std::ostream& out, std::optional<T> value) {
+std::ostream& Write(std::ostream& out, const std::optional<T>& value) {
   if (value.has_value()) {
     return Write(out, value.value());
   }
   return out << "<undefined>";
 }
 
-template <typename VALUE, typename... OTHERS>
-inline std::ostream& Write(std::ostream& out, VALUE&& value,
-                           OTHERS&&... others) {
-  Write(out, value);
-  Write(out, std::forward<OTHERS>(others)...);
+template <typename T>
+std::ostream& Write(std::ostream& out, const std::vector<T>& value) {
+  out << "[";
+  bool first = true;
+  for (const auto& el : value) {
+    if (!first) {
+      out << ", ";
+    }
+    first = false;
+    Write(out, el);
+  }
+  return out << "]";
+}
+
+template <typename K, typename V>
+std::ostream& Write(std::ostream& out, const std::unordered_map<K, V>& value) {
+  out << "{";
+  bool first = true;
+  for (auto it : value) {
+    if (!first) {
+      out << ", ";
+    }
+    first = false;
+    Write(out, it.first);
+    out << ": ";
+    Write(out, it.second);
+  }
+  return out << "}";
+}
+
+template <typename... TYS>
+std::ostream& Write(std::ostream& out, const std::variant<TYS...>& value) {
+  std::visit([&](auto&& v) { Write(out, v); }, value);
+  return out;
+}
+
+template <typename VALUE>
+std::ostream& Write(std::ostream& out, const VALUE& value) {
+  return out << value;
+}
+
+template <typename FIRST, typename SECOND, typename... REST>
+inline std::ostream& Write(std::ostream& out, FIRST&& first, SECOND&& second,
+                           REST&&... rest) {
+  Write(out, std::forward<FIRST>(first));
+  Write(out, std::forward<SECOND>(second));
+  Write(out, std::forward<REST>(rest)...);
   return out;
 }
 
