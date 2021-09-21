85a1f2713dee48068457cc70c0976a18fd4b502b
Ben Clayton <bclayton@google.com>
2021-09-21 15:49:01 +0100

    Document the IDL generator tool
    Rename `idl2cpp` to `idlgen`, as its really the template that dictates the output language.
    
    Add a license header

--- tools/cmd/idlgen/main.go
+++ tools/cmd/idlgen/main.go
@@ -1,3 +1,19 @@
+// Copyright 2021 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// idlgen is a tool used to generate code from WebIDL files and a golang
+// template file
 package main
 
 import (
@@ -22,13 +38,31 @@ func main() {
 	}
 }
 
+func showUsage() {
+	fmt.Println(`
+goidl is a tool used to generate code from WebIDL files and a golang
+template file
+
+Usage:
+  goidl --template=<template-path> --output=<output-path> <idl-file> [<idl-file>...]`)
+	os.Exit(1)
+}
+
 func run() error {
 	var templatePath string
 	var outputPath string
-	flag.StringVar(&templatePath, "template", "", "the template file to use with the generator")
+	flag.StringVar(&templatePath, "template", "", "the template file run with the parsed WebIDL files")
 	flag.StringVar(&outputPath, "output", "", "the output file")
 	flag.Parse()
 
+	idlFiles := flag.Args()
+
+	// Check all required arguments are provided
+	if templatePath == "" || outputPath == "" || len(idlFiles) == 0 {
+		showUsage()
+	}
+
+	// Open up the output file
 	out := os.Stdout
 	if outputPath != "" {
 		file, err := os.Create(outputPath)
@@ -39,14 +73,16 @@ func run() error {
 		defer file.Close()
 	}
 
+	// Read the template file
 	tmpl, err := ioutil.ReadFile(templatePath)
 	if err != nil {
 		return fmt.Errorf("failed to open template file '%v'", templatePath)
 	}
 
+	// idl is the combination of the parsed idlFiles
 	idl := &ast.File{}
 
-	idlFiles := flag.Args()
+	// Parse each of the WebIDL files and add the declarations to idl
 	for _, path := range idlFiles {
 		content, err := ioutil.ReadFile(path)
 		if err != nil {
@@ -63,10 +99,11 @@ func run() error {
 		idl.Declarations = append(idl.Declarations, fileIDL.Declarations...)
 	}
 
-	generated := &strings.Builder{}
+	// Initialize the generator
 	g := generator{t: template.New(templatePath)}
 	g.workingDir = filepath.Dir(templatePath)
 	g.funcs = map[string]interface{}{
+		// Functions exposed to the template
 		"AttributesOf":               attributesOf,
 		"ConstantsOf":                constantsOf,
 		"EnumEntryName":              enumEntryName,
@@ -98,17 +135,20 @@ func run() error {
 	if err != nil {
 		return fmt.Errorf("failed to parse template file '%v': %w", templatePath, err)
 	}
-	idl, declarations := sanitize(idl)
+
+	// simplify the definitions in the WebIDL before passing this to the template
+	idl, declarations := simplify(idl)
 	g.declarations = declarations
-	if err := t.Execute(generated, idl); err != nil {
-		return err
-	}
-	fmt.Fprint(out, generated.String())
-	return nil
+
+	// Execute the template
+	return t.Execute(out, idl)
 }
 
+// declarations is a map of WebIDL declaration name to its AST node.
 type declarations map[string]ast.Decl
 
+// nameOf returns the name of the AST node n.
+// panics if n is not a named node type.
 func nameOf(n ast.Node) string {
 	switch n := n.(type) {
 	case *ast.Interface:
@@ -128,13 +168,20 @@ func nameOf(n ast.Node) string {
 	}
 }
 
-func sanitize(in *ast.File) (*ast.File, declarations) {
-	s := sanitizer{
+// simplify processes the AST 'in', returning a new AST that:
+// * Has all partial interfaces merged into a single interface.
+// * Has all mixins flattened into their place of use.
+// * Has all the declarations ordered in dependency order (leaf first)
+// simplify also returns the map of declarations in the AST.
+func simplify(in *ast.File) (*ast.File, declarations) {
+	s := simplifier{
 		declarations: declarations{},
 		registered:   map[string]bool{},
 		out:          &ast.File{},
 	}
 
+	// Walk the IDL declarations to merge together partial interfaces and embed
+	// mixins into their uses.
 	{
 		interfaces := map[string]*ast.Interface{}
 		mixins := map[string]*ast.Mixin{}
@@ -177,22 +224,30 @@ func sanitize(in *ast.File) (*ast.File, declarations) {
 		}
 	}
 
+	// Now traverse the declarations in to produce the dependency-ordered
+	// output `s.out`.
 	for _, d := range in.Declarations {
 		if name := nameOf(d); name != "" {
-			s.declare(s.declarations[nameOf(d)])
+			s.visit(s.declarations[nameOf(d)])
 		}
 	}
 
 	return s.out, s.declarations
 }
 
-type sanitizer struct {
+// simplifier holds internal state for simplify()
+type simplifier struct {
+	// all AST declarations
 	declarations declarations
-	registered   map[string]bool
-	out          *ast.File
+	// set of visited declarations
+	registered map[string]bool
+	// the dependency-ordered output
+	out *ast.File
 }
 
-func (s *sanitizer) declare(d ast.Decl) {
+// visit traverses the AST declaration 'd' adding all dependent declarations to
+// s.out.
+func (s *simplifier) visit(d ast.Decl) {
 	register := func(name string) bool {
 		if s.registered[name] {
 			return true
@@ -206,13 +261,13 @@ func (s *sanitizer) declare(d ast.Decl) {
 			return
 		}
 		if d, ok := s.declarations[d.Inherits]; ok {
-			s.declare(d)
+			s.visit(d)
 		}
 		for _, m := range d.Members {
 			if m, ok := m.(*ast.Member); ok {
-				s.declareType(m.Type)
+				s.visitType(m.Type)
 				for _, p := range m.Parameters {
-					s.declareType(p.Type)
+					s.visitType(p.Type)
 				}
 			}
 		}
@@ -221,28 +276,28 @@ func (s *sanitizer) declare(d ast.Decl) {
 			return
 		}
 		if d, ok := s.declarations[d.Inherits]; ok {
-			s.declare(d)
+			s.visit(d)
 		}
 		for _, m := range d.Members {
-			s.declareType(m.Type)
+			s.visitType(m.Type)
 			for _, p := range m.Parameters {
-				s.declareType(p.Type)
+				s.visitType(p.Type)
 			}
 		}
 	case *ast.Typedef:
 		if register(d.Name) {
 			return
 		}
-		s.declareType(d.Type)
+		s.visitType(d.Type)
 	case *ast.Mixin:
 		if register(d.Name) {
 			return
 		}
 		for _, m := range d.Members {
 			if m, ok := m.(*ast.Member); ok {
-				s.declareType(m.Type)
+				s.visitType(m.Type)
 				for _, p := range m.Parameters {
-					s.declareType(p.Type)
+					s.visitType(p.Type)
 				}
 			}
 		}
@@ -261,40 +316,51 @@ func (s *sanitizer) declare(d ast.Decl) {
 	s.out.Declarations = append(s.out.Declarations, d)
 }
 
-func (s *sanitizer) declareType(t ast.Type) {
+// visitType traverses the AST type 't' adding all dependent declarations to
+// s.out.
+func (s *simplifier) visitType(t ast.Type) {
 	switch t := t.(type) {
 	case *ast.TypeName:
 		if d, ok := s.declarations[t.Name]; ok {
-			s.declare(d)
+			s.visit(d)
 		}
 	case *ast.UnionType:
 		for _, t := range t.Types {
-			s.declareType(t)
+			s.visitType(t)
 		}
 	case *ast.ParametrizedType:
 		for _, t := range t.Elems {
-			s.declareType(t)
+			s.visitType(t)
 		}
 	case *ast.NullableType:
-		s.declareType(t.Type)
+		s.visitType(t.Type)
 	case *ast.SequenceType:
-		s.declareType(t.Elem)
+		s.visitType(t.Elem)
 	case *ast.RecordType:
-		s.declareType(t.Elem)
+		s.visitType(t.Elem)
 	default:
 		panic(fmt.Errorf("unhandled AST type %T", t))
 	}
 }
 
+// generator holds the template generator state
 type generator struct {
-	t            *template.Template
-	workingDir   string
-	funcs        map[string]interface{}
+	// the root template
+	t *template.Template
+	// the working directory
+	workingDir string
+	// map of function name to function exposed to the template executor
+	funcs map[string]interface{}
+	// dependency-sorted declarations
 	declarations declarations
 }
 
-// eval executes the sub-template with the given name and argument, returning
+// eval executes the sub-template with the given name and arguments, returning
 // the generated output
+// args can be a single argument:
+//   arg[0]
+// or a list of name-value pairs:
+//   (args[0]: name, args[1]: value), (args[2]: name, args[3]: value)...
 func (g *generator) eval(template string, args ...interface{}) (string, error) {
 	target := g.t.Lookup(template)
 	if target == nil {
@@ -324,10 +390,13 @@ func (g *generator) eval(template string, args ...interface{}) (string, error) {
 	return sb.String(), nil
 }
 
+// lookup returns the declaration with the given name, or nil if not found.
 func (g *generator) lookup(name string) ast.Decl {
 	return g.declarations[name]
 }
 
+// include loads the template with the given path, importing the declarations
+// into the scope of the current template.
 func (g *generator) include(path string) (string, error) {
 	t, err := g.t.
 		Option("missingkey=invalid").
@@ -386,15 +455,20 @@ func isUndefinedType(ty ast.Type) bool {
 	return false
 }
 
+// enumEntryName formats the enum entry name 's' for use in a C++ enum.
 func enumEntryName(s string) string {
 	return "k" + strings.ReplaceAll(pascalCase(strings.Trim(s, `"`)), "-", "")
 }
 
+// Method describes a WebIDL interface method
 type Method struct {
-	Name      string
+	// Name of the method
+	Name string
+	// The list of overloads of the method
 	Overloads []*ast.Member
 }
 
+// methodsOf returns all the methods of the given WebIDL interface.
 func methodsOf(iface *ast.Interface) []*Method {
 	byName := map[string]*Method{}
 	out := []*Method{}
@@ -416,6 +490,7 @@ func methodsOf(iface *ast.Interface) []*Method {
 	return out
 }
 
+// attributesOf returns all the attributes of the given WebIDL interface.
 func attributesOf(iface *ast.Interface) []*ast.Member {
 	out := []*ast.Member{}
 	for _, m := range iface.Members {
@@ -427,6 +502,7 @@ func attributesOf(iface *ast.Interface) []*ast.Member {
 	return out
 }
 
+// constantsOf returns all the constant values of the given WebIDL interface.
 func constantsOf(iface *ast.Interface) []*ast.Member {
 	out := []*ast.Member{}
 	for _, m := range iface.Members {
