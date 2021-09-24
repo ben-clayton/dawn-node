a7435f4441522751f05151ea9d7959ce51a995fb
Antonio Maiorano <amaiorano@google.com>
2021-09-24 15:49:50 -0400

    Fix for Linux

--- tools/gpuweb_cts_runner/gpuweb_cts_runner.py
+++ tools/gpuweb_cts_runner/gpuweb_cts_runner.py
@@ -6,44 +6,32 @@ import sys
 import subprocess
 from subprocess import TimeoutExpired, check_output, CalledProcessError
 import multiprocessing as mp
-from multiprocessing import Pool, Lock
+from multiprocessing import Pool
 import signal
 from pathlib import Path, PurePosixPath
 from timeit import default_timer as timer
 
-proc_pool_size = mp.cpu_count()
+# Tweakables
+proc_pool_size = 10 #mp.cpu_count()
 test_timeout_sec = 60*5
+dawnnode_module = 'build/dawnnode.node'
+# dawnnode_module = 'build/RelWithDebInfo/dawnnode.node'
+all_tests_file = 'all_tests.txt'
+# all_tests_file = 'single_test.txt'
+
 
 script_dir = Path(__file__).resolve().parent
 gpuweb_cts_dir = script_dir.joinpath('../../../gpuweb-cts').resolve()
 dawnnode_dir = script_dir.parent.parent.resolve()
-
 run_dawn_node_js = script_dir / 'run-dawn-node.js'
-
-all_tests_path = script_dir / 'all_tests.txt'
-# all_tests_path = script_dir / 'single_test.txt'
-
-# tests_fail_path = script_dir / 'tests_fail.txt'
-# tests_pass_path = script_dir / 'tests_pass.txt'
-# tests_skip_path = script_dir / 'tests_skip.txt'
-# tests_unknown_path = script_dir / 'tests_unknown.txt'
-# tests_timeout_path = script_dir / 'tests_timeout.txt'
-
-lock = Lock()
-
-# def append_to_file(filename, text):
-#     with lock:
-#         with open(filename, 'a+') as f:
-#             f.write(text)
-#             if not text.endswith('\n'):
-#                 f.write('\n')
-
+all_tests_path = script_dir / all_tests_file
 
 def start_test_process(test_name, timeout_sec = 0):
-    test_name = test_name.replace(r'"', r'\"') # Escape quotes or shell won't be happy
-    cmd = 'node {} --verbose {}'.format(run_dawn_node_js, test_name)
+    if os.name == 'nt':
+        test_name = test_name.replace(r'"', r'\"') # Escape quotes or shell won't be happy
+    cmd_args = ['node', run_dawn_node_js, '--verbose', test_name]
     try:
-        output = check_output(cmd, timeout=timeout_sec, stderr=subprocess.STDOUT, text=True, cwd=gpuweb_cts_dir)
+        output = check_output(args=cmd_args, timeout=timeout_sec, stderr=subprocess.STDOUT, text=True, cwd=gpuweb_cts_dir)
         return output
     except CalledProcessError as cpe:
         return cpe.output
@@ -52,7 +40,7 @@ def start_test_process(test_name, timeout_sec = 0):
 def run_test(test_name, results):
     # Run process in cts dir
     test_name = test_name.strip()
-    
+
     start_time = timer()
     def end_time():
         return timer() - start_time
@@ -62,7 +50,6 @@ def run_test(test_name, results):
     except TimeoutExpired:
         print(f'{end_time():.2f}s [TIMEOUT]: {test_name}')
         results['timeout'].append(test_name)
-        # append_to_file(tests_timeout_path, test_name)
         return
 
     found_result = False
@@ -70,26 +57,22 @@ def run_test(test_name, results):
         line = line.strip()
         if line.startswith('[fail]'):
             print(f'{end_time():.2f}s {line}')
-            # append_to_file(tests_fail_path, test_name)
             results['fail'].append(test_name)
             found_result = True
             break
         elif line.startswith('[pass]'):
             print(f'{end_time():.2f}s {line}')
-            # append_to_file(tests_pass_path, test_name)
             results['pass'].append(test_name)
             found_result = True
             break
         elif line.startswith('[skip]'):
             print(f'{end_time():.2f}s {line}')
-            # append_to_file(tests_skip_path, test_name)
             results['skip'].append(test_name)
             found_result = True
             break
     if not found_result:
         print(f'{end_time():.2f}s [UNKNOWN]: {test_name}')
         print(output)
-        # append_to_file(tests_unknown_path, test_name)
         results['unknown'].append(test_name)
 
 
@@ -118,13 +101,7 @@ def main():
     start_time = timer()
 
     os.chdir(sys.path[0]) # Run in script dir
-    # clear_file_contents(tests_fail_path)
-    # clear_file_contents(tests_pass_path)
-    # clear_file_contents(tests_skip_path)
-    # clear_file_contents(tests_unknown_path)
-    # clear_file_contents(tests_timeout_path)
 
-    dawnnode_module = 'build/RelWithDebInfo/dawnnode.node'
     generate_run_dawnnode_js(dawnnode_module)
 
     all_tests = []
@@ -147,7 +124,7 @@ def main():
         for (type, values) in results.items():
             file = script_dir / f'tests_{type}.txt'
             clear_file_contents(file)
-        
+
         args = [(a,results) for a in all_tests]
         pool.starmap(run_test, args)
     except KeyboardInterrupt:
@@ -158,9 +135,9 @@ def main():
         pool.close()
     pool.join()
 
-    # Output results, sorted into files
     for (type, values) in results.items():
         file = script_dir / f'tests_{type}.txt'
+        print(f'Writing sorted results to: {file}')
         with open(file, 'w') as f:
             values.sort()
             for v in values:
