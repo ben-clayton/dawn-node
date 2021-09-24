f36e509f03035161bc2c49ba2ae669df15c2863c
Antonio Maiorano <amaiorano@google.com>
2021-09-24 13:29:41 -0400

    gpuweb_cts_runner.py: make it runnable from outside the cts root

--- tools/gpuweb_cts_runner/gpuweb_cts_runner.py
+++ tools/gpuweb_cts_runner/gpuweb_cts_runner.py
@@ -1,24 +1,38 @@
 # gpuweb_cts_runner.py
 # author: amaiorano@
 
-import io
 import os
+import sys
 import subprocess
 from subprocess import TimeoutExpired, check_output, CalledProcessError
 from multiprocessing import Pool, Lock
 import signal
+from pathlib import Path, PurePosixPath
+from timeit import default_timer as timer
+
+proc_pool_size = 100
+test_timeout_sec = 120
+
+script_dir = Path(__file__).resolve().parent
+gpuweb_cts_dir = script_dir.joinpath('../../../gpuweb-cts').resolve()
+dawnnode_dir = script_dir.parent.parent.resolve()
+
+run_dawn_node_js = script_dir / 'run-dawn-node.js'
+
+all_tests_path = script_dir / 'all_tests.txt'
+# all_tests_path = script_dir / 'single_test.txt'
+
+tests_fail_path = script_dir / 'tests_fail.txt'
+tests_pass_path = script_dir / 'tests_pass.txt'
+tests_skip_path = script_dir / 'tests_skip.txt'
+tests_unknown_path = script_dir / 'tests_unknown.txt'
+tests_timeout_path = script_dir / 'tests_timeout.txt'
 
 lock = Lock()
 
-test_timeout_sec = 120
-
-# append_to_files_set = set()
 def append_to_file(filename, text):
     with lock:
-        # mode = 'a' if filename in append_to_files_set else 'w'
-        # append_to_files_set.add(filename)
-        mode = 'a+'
-        with open(filename, mode) as f:
+        with open(filename, 'a+') as f:
             f.write(text)
             if not text.endswith('\n'):
                 f.write('\n')
@@ -26,80 +40,105 @@ def append_to_file(filename, text):
 
 def start_test_process(test_name, timeout_sec = 0):
     test_name = test_name.replace(r'"', r'\"') # Escape quotes or shell won't be happy
-    # return subprocess.Popen('node run-dawn-node.js --verbose ' + test_name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    cmd = 'node run-dawn-node.js --verbose ' + test_name
+    cmd = 'node {} --verbose {}'.format(run_dawn_node_js, test_name)
     try:
-        output = check_output(cmd, timeout=timeout_sec, stderr=subprocess.STDOUT, text=True)
+        output = check_output(cmd, timeout=timeout_sec, stderr=subprocess.STDOUT, text=True, cwd=gpuweb_cts_dir)
         return output
     except CalledProcessError as cpe:
         return cpe.output
 
 
 def run_test(test_name):
+    # Run process in cts dir
     test_name = test_name.strip()
+    
+    start_time = timer()
+    def end_time():
+        return timer() - start_time
 
     try:
         output = start_test_process(test_name, timeout_sec=test_timeout_sec)
     except TimeoutExpired:
-        print('[TIMEOUT]: ' + test_name)
-        append_to_file('tests_timeout.txt', test_name)
+        print(f'{end_time()}s [TIMEOUT]: {test_name}')
+        append_to_file(tests_timeout_path, test_name)
         return
 
     found_result = False
-    # for line in io.TextIOWrapper(output, encoding="utf-8"):
     for line in output.splitlines():
         line = line.strip()
         if line.startswith('[fail]'):
-            print(line)
-            append_to_file('tests_fail.txt', test_name)
+            print(f'{end_time()}s {line}')
+            append_to_file(tests_fail_path, test_name)
             found_result = True
             break
         elif line.startswith('[pass]'):
-            print(line)
-            append_to_file('tests_pass.txt', test_name)
+            print(f'{end_time()}s {line}')
+            append_to_file(tests_pass_path, test_name)
             found_result = True
             break
         elif line.startswith('[skip]'):
-            print(line)
-            append_to_file('tests_skip.txt', test_name)
+            print(f'{end_time()}s {line}')
+            append_to_file(tests_skip_path, test_name)
             found_result = True
             break
     if not found_result:
-        print('[UNKNOWN]: ' + test_name)
-        append_to_file('tests_unknown.txt', test_name)
-
-def init_worker():
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
+        print(f'{end_time()}s [UNKNOWN]: {test_name}')
+        print(output)
+        append_to_file(tests_unknown_path, test_name)
 
 
-def delete_file(f):
-    if os.path.exists(f):
-        os.remove(f)
+def clear_file_contents(f):
+    p = Path(f)
+    if p.exists():
+        p.unlink()
+    p.touch()
+
+
+def to_unix(p):
+    return str(p).replace('\\', '/')
+
+
+def generate_run_dawnnode_js(dawnnode_module):
+    with open(run_dawn_node_js, 'w') as f:
+        source = f"""
+        require('{dawnnode_dir}/{dawnnode_module}')
+        require('{gpuweb_cts_dir}/src/common/tools/setup-ts-in-node.js');
+        require('{gpuweb_cts_dir}/src/common/runtime/cmdline.ts');
+        """
+        f.write(to_unix(source))
+
 
 def main():
-    delete_file('tests_fail.txt') 
-    delete_file('tests_pass.txt')
-    delete_file('tests_skip.txt')
-    delete_file('tests_unknown.txt')
-    delete_file('tests_timeout.txt')
+    start_time = timer()
+
+    os.chdir(sys.path[0]) # Run in script dir
+    clear_file_contents(tests_fail_path)
+    clear_file_contents(tests_pass_path)
+    clear_file_contents(tests_skip_path)
+    clear_file_contents(tests_unknown_path)
+    clear_file_contents(tests_timeout_path)
+
+    dawnnode_module = 'build/RelWithDebInfo/dawnnode.node'
+    generate_run_dawnnode_js(dawnnode_module)
 
     all_tests = []
-    with open('all_tests.txt', 'r') as f:
-    # with open('single_test.txt', 'r') as f:
+    with open(all_tests_path, 'r') as f:
         all_tests = f.readlines()
 
-    #original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
-    pool = Pool(100, init_worker)
-    #signal.signal(signal.SIGINT, original_sigint_handler)
+    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    pool = Pool(proc_pool_size)
+    signal.signal(signal.SIGINT, original_sigint_handler)
     try:
         pool.map(run_test, all_tests)
     except KeyboardInterrupt:
-        print("Caught KeyboardInterrupt, terminating workers")
+        print("Caught KeyboardInterrupt, terminating processes")
         pool.terminate()
     else:
-        print("Normal termination")
+        print("Terminated normally")
         pool.close()
     pool.join()
+
+    print(f'Total elapsed time: {timer() - start_time}s')
 
 if __name__ == '__main__':
     main()
